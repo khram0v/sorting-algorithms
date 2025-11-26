1b7b7dd10054e424ed7c994f62eb4693b4a7b5b4
Sviatoslav Khramov <kent8352@gmail.com>
2025-11-26 19:30:31 +0200

    Add tests for 3 sorting algorithms
    - Created test classes for `BubbleSort`, `SelectionSort` and `InsertionSort`
    - Each sorting algorithm is tested equally with common edge cases

---
+++ src/test/java/io/github/khram0v/sorting/InsertionSortTest.java
@@ -0,0 +1,45 @@
+package io.github.khram0v.sorting;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InsertionSortTest {
+    InsertionSort<Integer> sort;
+
+    @BeforeEach
+    void setUp() {
+        sort = new InsertionSort<>();
+    }
+
+    @Test
+    void sort_unsortedList_returnsSortedList() {
+        List<Integer> list = new ArrayList<>(List.of(3, 1, 2));
+
+        sort.sort(list);
+
+        assertEquals(List.of(1, 2, 3),  list);
+    }
+
+    @Test
+    void sort_sortedList_remainsSorted() {
+        List<Integer> list = new ArrayList<>(List.of(1, 2, 3));
+
+        sort.sort(list);
+
+        assertEquals(List.of(1, 2, 3), list);
+    }
+
+    @Test
+    void sort_listWithDuplicates_sortCorrectly() {
+        List<Integer> list = new ArrayList<>(List.of(2, 3, 4, 1, 4, 2));
+
+        sort.sort(list);
+
+        assertEquals(List.of(1, 2, 2, 3, 4, 4), list);
+    }
+}
